2fe9d53e491ab69a0cad4130ef6c4414aa277361
Holmesen <1315654996@qq.com>
2022-11-28 18:21:48 +0800

    feat: 添加了AntDesign组件库

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
+import { Button } from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
 
-app.use(router).use(store);
+app.use(router).use(store).use(Button);
 
 app.mount('#app');
