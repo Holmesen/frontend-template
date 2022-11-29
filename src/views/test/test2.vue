f60e269f68e74e9933c9f57f3270940532b00b26
Holmesen <1315654996@qq.com>
2022-11-29 21:40:21 +0800

    test: 添加测试文件

---
+++ src/views/test/test2.vue
@@ -0,0 +1,45 @@
+<!--  -->
+<template>
+  <button>
+    <span class="button_top"> Button </span>
+  </button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="less">
+/* Variables */
+@button_radius: 0.75em;
+@button_color: #e8e8e8;
+@button_outline_color: #000;
+
+button {
+  font-size: 17px;
+  font-weight: bold;
+  background: var(@button_outline_color);
+  border: none;
+  border-radius: var(@button_radius);
+}
+
+.button_top {
+  box-sizing: border-box;
+  display: block;
+  padding: 0.75em 1.5em;
+  color: var(@button_outline_color);
+  background: var(@button_color);
+  border: 2px solid var(@button_outline_color);
+  border-radius: var(@button_radius);
+  transition: transform 0.1s ease;
+  transform: translateY(-0.2em);
+}
+
+button:hover .button_top {
+  /* Pull the button upwards when hovered */
+  transform: translateY(-0.33em);
+}
+
+button:active .button_top {
+  /* Push the button downwards when pressed */
+  transform: translateY(0);
+}
+</style>
